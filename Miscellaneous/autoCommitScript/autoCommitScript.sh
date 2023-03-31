e0b94744293887fce6b8f8e1f11621797e277393
Nitkarsh Chourasia <playnitkarsh@gmail.com>
2023-03-31 18:24:47 +0530

    newCommits @ 2023-03-31-18:24:47

--- Miscellaneous/autoCommitScript/autoCommitScript.sh
+++ Miscellaneous/autoCommitScript/autoCommitScript.sh
@@ -2,7 +2,7 @@
 
 # Script made by me, Nitkarsh Chourasia.
 
-cd /home/nitkarshc/Desktop/funPython;
+cd /home/nitkarshc/Desktop/funPy;
 git add --all;
 git commit -S -m "newCommits @ `date +%F-%T`";
 git push;
